79f9b956a013338b40bab74bf5fe7f64020d1c04
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleIdentityAccessContextManagerV1/ServicePerimeter.swift
+++ Sources/GoogleIdentityAccessContextManagerV1/ServicePerimeter.swift
@@ -200,8 +200,8 @@ public struct ServicePerimeter: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .regular: return try container.encode(0)
-      case .bridge: return try container.encode(1)
+      case .regular: return try container.encode("PERIMETER_TYPE_REGULAR")
+      case .bridge: return try container.encode("PERIMETER_TYPE_BRIDGE")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
