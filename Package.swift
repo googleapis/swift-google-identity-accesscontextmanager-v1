2018e2245653eca366470e29fbcd3d1231a2070c
Jeff Ching <chingor@google.com>
2026-09-16 21:13:51 +0000

    refactor(auth)!: rename GoogleCloudAuth to GoogleAuth (#920)
    Fixes #905

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
     .target(
       name: "GoogleIdentityAccessContextManagerV1",
       dependencies: [
-        .product(name: "GoogleCloudAuth", package: "swift-google-auth"),
+        .product(name: "GoogleAuth", package: "swift-google-auth"),
         .product(name: "GoogleCloudGax", package: "swift-google-gax"),
         .product(name: "GoogleCloudWKT", package: "swift-google-wkt"),
         .product(name: "GoogleIAMV1", package: "swift-google-iam-v1"),
